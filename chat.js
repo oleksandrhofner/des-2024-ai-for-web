6272b3c1d4923e9a2755acf493a73aa08af6bcbf
Oleksandr Hofner <argamakov2213@gmail.com>
2024-01-24 16:29:19 +0200

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,59 @@
+import OpenAI from "openai";
+import readline from "node:readline";
+
+//const openai = new OpenAI();
+
+// Local API
+const openai = new OpenAI({
+    baseURL: 'http://localhost:1234/v1',
+    apiKey: 'not-needed',
+  })
+
+  async function generateResponse(question, chatHistory) {
+    chatHistory.push({
+        role: "user",
+        content: question
+    });
+
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo-16k",
+        temperature: 0,
+        messages: chatHistory,
+        max_tokens: 1500
+    });
+
+    const aiResponseMessage = response.choices[0].message.content;
+    
+    chatHistory.push({
+        role: "assistant",
+        content: aiResponseMessage
+    });
+
+    return aiResponseMessage;
+}
+
+async function chatLoop() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    }); 
+    
+    const askQuestion = (query) =>
+       new Promise((resolve) => rl.question(query, resolve));
+
+    const chatHistory = [
+        {
+            role: "system",
+            content: "You are helpful AI assistant, please follow instructions"
+        }
+    ];
+    while (true) {
+        const question = await askQuestion('Please ask question (type "exit" to quit)> ');
+        if (question.toLowerCase() === 'exit') break;
+        const answer = await generateResponse(question, chatHistory);
+        console.log(answer);
+    }    
+    rl.close();
+}
+
+await chatLoop();
